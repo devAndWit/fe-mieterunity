8a18413d6481f6ec9b3a569915493a8b865acf80
Andre Witt <dev.code@h-town.net>
2025-02-25 15:55:18 +0100

    create AccountForm

---
+++ src/components/Account/Account.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import styles from "./Account.module.css";
+
+export const Account = () => {
+  return (
+    <main>
+      <h1>Konto</h1>
+      <section>
+        <form action="" method="POST" className={styles.AccountForm}>
+          <p>
+            <label htmlFor="firstname">Vorname:</label>
+            <input type="text" name="firstname" id="firstname" />
+          </p>
+
+          <p>
+            <label htmlFor="lastname">Nachname:</label>
+            <input type="text" name="lastname" id="lastname" />
+          </p>
+
+          <p>
+            <label htmlFor="phone">Phone:</label>
+            <input type="text" name="phone" id="phone" />
+          </p>
+
+          <p>
+            <button type="button">Speichern</button>
+            <Link to="/profile">Close</Link>
+          </p>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default Account;
